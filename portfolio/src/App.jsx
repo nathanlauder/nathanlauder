85fe6ff9b33ccf96a6be88fc05cb76aa37c025eb
nathanlauder <laudern42@gmail.com>
2022-04-22 07:35:59 -0500

    wip--not a huge fan of this

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './App.css';
 import Navigation from './Sections/Navigation';
 import Landing from './Sections/Landing';
-import Experience from './Sections/Experience';
-import Projects from './Sections/Projects';
-import Tools from './Sections/Tools';
-import Education from './Sections/Education';
-import Footer from './Sections/Footer';
+// import Experience from './Sections/Experience';
+// import Projects from './Sections/Projects';
+// import Tools from './Sections/Tools';
+// import Education from './Sections/Education';
+// import Footer from './Sections/Footer';
 
 function App() {
   return (
     <div id="app">
       <Navigation />
       <Landing />
-      <Experience />
+      {/* <Experience />
       <Projects />
       <Tools />
       <Education />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- portfolio/src/Sections/Landing.jsx
+++ portfolio/src/Sections/Landing.jsx
@@ -4,25 +4,23 @@ import { TextLoop } from 'react-text-loop-next';
 
 const Landing = () => (
   <Container id="landing">
+    <Headshot>
+      <img src="imgs/2021HeadshotBW.png" alt="Nathan Lauder" />
+    </Headshot>
     <Greeting>
-      <div id="headshot">
-        <img src="imgs/headshot.jpg" alt="Nathan Lauder" />
-      </div>
-      <div id="welcome">
-        Welcome!
-        <br />
-        I'm Nathan, and I'm a
-        <div id="loop">
-          <TextLoop
-            children={['student', 'developer', 'problem solver', 'self-learner', 'forward thinker']}
-            interval={5000}
-            fade
-            springConfig={{
-              stiffness: 100,
-              damping: 10
-            }}
-          />
-        </div>
+      Welcome!
+      <br />
+      I'm Nathan, and I'm a
+      <div id="loop">
+        <TextLoop
+          children={['student', 'developer', 'problem solver', 'self-learner', 'forward thinker']}
+          interval={5000}
+          fade
+          springConfig={{
+            stiffness: 100,
+            damping: 10
+          }}
+        />
       </div>
     </Greeting>
   </Container>
@@ -30,51 +28,19 @@ const Landing = () => (
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  height: 98vh;
-  padding: 1rem;
-  background-image: url('imgs/space.jpg');
-  background-size: cover;
-  background-repeat: no-repeat;
-  @media (max-width: 600px) {
-    margin-left: 1rem;
-  }
+  background-color: #000;
 `;
 
-const Greeting = styled.div`
-  margin-left: 2rem;
-  #loop div {
-    color: #ff00d4;
-    font-size: 2rem;
-  }
-  div#welcome {
-    text-align: center;
-    font-size: 2rem;
-    letter-spacing: 3;
-    line-height: 1.2;
-  }
-  #headshot  {
+const Headshot = styled.div`
     margin: 1rem;
-    text-align: center;
+    width: 40%;
     img {
-      border-radius: 50%;
-      border: 2.5pt solid#d7d7d7;
-      width: 15rem;
+      width: 100%;
     }
-  }
-
-  @media (max-width: 600px) {
-    #loop div {
-      font-size: 1.5rem;
-    }
-    div#welcome {
-      font-size: 1.5rem;
-    }
-    #headshot img {
-      width: 8rem;
-    }
-  }
 `;
+
+const Greeting = styled.div``;
 
 export default Landing;
