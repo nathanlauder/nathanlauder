1083912cafe3e59680d18c459d5572043214549d
nathanlauder <laudern42@gmail.com>
2022-05-09 17:12:43 -0500

    use hex for default

--- portfolio/src/Components/Link.js
+++ portfolio/src/Components/Link.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Link = styled.a`
   font-size: ${({ size }) => size || 1}rem;
   text-decoration: none;
-  color: ${({ color }) => color || 'white'};
+  color: ${({ color }) => color || '#ffffff'};
 `;
 
 export default Link;
