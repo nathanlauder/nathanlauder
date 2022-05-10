9eb40985a8aac8080d23c64514001ec4aa653052
nathanlauder <laudern42@gmail.com>
2022-05-10 15:15:26 -0500

    create mobile responsive nav

--- portfolio/src/Sections/Navigation.jsx
+++ portfolio/src/Sections/Navigation.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import React from 'react';
 import Link from '../Components/Link';
+import HamMenu from '../Components/HamMenu';
+import Nav from '../Components/Nav';
+import NavOption from '../Components/NavOption';
 
 const Navigation = () => (
   <Container>
@@ -11,74 +14,55 @@ const Navigation = () => (
     </Logo>
     <Nav>
       <NavOption>
-        <Link href="#experience" color="#ffd700">Experience</Link>
+        <Link href="#experience">Experience</Link>
       </NavOption>
       <NavOption>
-        <Link href="#projects" color="#ffd700">Projects</Link>
+        <Link href="#projects">Projects</Link>
       </NavOption>
       <NavOption>
-        <Link href="#toolkit" color="#ffd700">Tools</Link>
+        <Link href="#toolkit">Tools</Link>
       </NavOption>
       <NavOption>
-        <Link href="#education" color="#ffd700">Education</Link>
+        <Link href="#education">Education</Link>
       </NavOption>
       <NavOption>
-        <Link href="#footer" color="#ffd700">Contact</Link>
+        <Link href="#footer">Contact</Link>
       </NavOption>
     </Nav>
+    <HamMenu />
   </Container>
 );
 
 const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    margin-right: 1rem;
+  }
 `;
 
 const Logo = styled.div`
-    width: 2.3rem;
-    margin-left: 3rem;
-    
-    img {
-      animation: spinLogo 4s infinite ease-in-out;
-      width: 100%;
-      @keyframes spinLogo {
-          40% {
-          transform: rotate(0deg);
-          }
-          100% {
-          transform: rotate(360deg);
-          }
-      }
+  width: 2.3rem;
+  margin-left: 2rem;
+  
+  img {
+    animation: spinLogo 4s infinite ease-in-out;
+    width: 100%;
+    @keyframes spinLogo {
+        40% {
+        transform: rotate(0deg);
+        }
+        100% {
+        transform: rotate(360deg);
+        }
     }
-    @media (max-width: 600px) {
-      width: 1.9rem;
-      padding: 0.3rem;
-    }
-`;
-
-const Nav = styled.div`
-  display: flex;
-`;
-
-const NavOption = styled.div`
-  padding: 1.5rem 1rem;
-
-  :not(:last-child) {
-    border-right: 1px solid #ffd700;
   }
 
-  ${Link} {
-    font-size: 1.1rem;
-  }
-
-  :hover {
-    background-color: white;
-    cursor: pointer;
-    ${Link} {
-      color: black;
-    }
+  @media screen and (max-width: 600px) {
+    width: 2rem;
+    padding-top: 5px;
   }
 `;
 
