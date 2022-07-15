dff816af6c5d106437a1984567eeb0d55cd6f9d3
nathanlauder <laudern42@gmail.com>
2022-07-15 13:58:20 -0500

    use default colors

--- portfolio/src/Components/SectionTitle.js
+++ portfolio/src/Components/SectionTitle.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
+import colors from '../util/colors';
 
 const SectionTitle = styled.div`
   font-size: 2rem;
   font-weight: 700;
   letter-spacing: 2px;
   text-align: center;
-  margin: 2rem 0;  
-  color: var(--capri);  
+  margin: ${({ margin }) => margin || '2rem'} 0;  
+  color: ${({ color }) => color || colors.black};  
 `;
 
 export default SectionTitle;
