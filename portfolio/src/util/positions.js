4523891047e5ee05a01ea830fe93e98ece32bc6a
Nathan Lauder <laudern42@gmail.com>
2023-12-14 15:41:04 -0600

    add position descriptions

---
+++ portfolio/src/util/positions.js
@@ -0,0 +1,14 @@
+module.exports = {
+  jpmc: {
+    description: 'At JPMorgan Chase, I work in Corporate and Investment Banking to develop software for both internal and external clients.',
+    skills: ['Java', 'Spring Boot', 'TypeScript', 'ReactJS', 'Kafka', 'Kubernetes', 'Docker']
+  },
+  dmsi: {
+    description: 'At DMSi, I worked with a team in an effort to migrate a legacy Windows application to the web.  During this time, I leveraged my knowledge of React while learning a little bit of Go.',
+    skills: ['ReactJS', 'Golang', 'GraphQL']
+  },
+  gehc: {
+    description: 'At GE Healthcare, I worked on a backend integration between two management systems.',
+    skills: ['JavaScript', 'NodeJS', 'ExpressJS']
+  }
+};
